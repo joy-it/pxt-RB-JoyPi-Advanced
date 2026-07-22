4b8f17aec40446b016026231a07af5efd5ad8107
kendra-eck <206059012+kendra-eck@users.noreply.github.com>
2026-07-22 11:16:37 +0200

    feat: Add recognized magnetic field to hall sensor

--- HallSensor.ts
+++ HallSensor.ts
@@ -1,14 +1,33 @@
 namespace JoyPiAdvanced {
-
-  /**
-   * Measures the voltage of the hall sensor. A higher value means a higher amplitude
-   */
-    //% block="hall sensor voltage"
+    /**
+     * returns digital value of hall sensor
+     */
+    //% block="hall sensor digital value"
     //% subcategory="Hall sensor"
     //% weight=100
-    export function hallSensorVoltage(): number {
-      return adcReadVoltage(5)
+    export function hallSensorGetMagneticField() {
+      return adcReadValue(4)
+    } 
+
+    /**
+     * returns true if value of hall sensor fits north pole
+     */
+    //% block="%value is a north pole"
+    //% subcategory="Hall sensor"
+    //% weight=90
+    export function hallSensorIsNorthPole(value: number){
+        if (value < 1800) return true
+        else return false
     }
-  
-  }
-  
+
+    /**
+     * returns true if value of hall sensor fits south pole
+     */
+    //% block="%value is a south pole"
+    //% subcategory="Hall sensor"
+    //% weight=80
+    export function hallSensorIsSouthPole(value: number) {
+        if (value > 2400) return true
+        else return false
+    }
+}
